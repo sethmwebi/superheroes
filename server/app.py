11259413d5cb52bfbf42f9c365bac8bd3238fd50
Seth Mwebi <sethmwebi27@gmail.com>
2024-10-07 10:54:11 +0300

    sets up views for /heroes

--- server/app.py
+++ server/app.py
@@ -1,28 +1,35 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, make_response
-from flask_migrate import Migrate
-from flask_restful import Api, Resource
-from models import db, Hero, Power, HeroPower
 import os
 
+from flask import Flask, jsonify, make_response, request
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from models import Hero, HeroPower, Power, db
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE = os.environ.get(
-    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+DATABASE = os.environ.get("DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return '<h1>Code challenge</h1>'
+    return "<h1>Code challenge</h1>"
 
 
-if __name__ == '__main__':
+@app.route("/heroes", methods=["GET"])
+def get_heroes():
+    heroes = Hero.query.all()
+    return jsonify([hero.to_dict(rules=("-hero_powers",)) for hero in heroes]), 200
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
